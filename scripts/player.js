e37e1c2792862c737a781dab9fdf6273ac6167eb
MirstT <W_H_Norway@outlook.com>
2021-10-18 23:31:39 +0800

    18

--- scripts/player.js
+++ scripts/player.js
@@ -4,5 +4,33 @@
  * @Author: Mirst
  * @Date: 2021-10-16 13:31:41
  * @LastEditors: Mirst
- * @LastEditTime: 2021-10-16 13:31:42
+ * @LastEditTime: 2021-10-18 17:28:37
  */
+
+
+function createVideoControls(){
+    var vids = document.getElementsByTagName("video");
+    for(var i=0;i<vids.length;i++){
+        addControls(vids[i]);
+    }
+}
+
+function addControls(vid){
+    vid.removeAttribute("controls");
+
+    vid.height=vid.videoHeight;
+    vid.width=vid.videoWidth;
+
+    vid.parentNode.style.height=vid.videoHeight+"px";
+    vid.parentNode.style.width=vid.videoWidth+"px";
+
+    var controls = document.createElement("div");
+    controls.setAttribute("class","controls");
+
+    var p;
+}
+
+
+window.onload=function(){
+    createVideoControls();
+}
