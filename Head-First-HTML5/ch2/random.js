856321dcf9d2f009470cf8cb30e00d22993e981a
MirstT <W_H_Norway@outlook.com>
2021-10-19 16:23:06 +0800

    ch1 ch2

---
+++ Head-First-HTML5/ch2/random.js
@@ -0,0 +1,11 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: Mirst
+ * @Date: 2021-10-19 16:20:58
+ * @LastEditors: Mirst
+ * @LastEditTime: 2021-10-19 16:22:02
+ */
+
+var array=new Array(100);
+array[Math.floor(Math.random()*array.length)];
