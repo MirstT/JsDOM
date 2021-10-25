ddc0ac12e650683050dde55e9a95b14399215aa1
MirstT <W_H_Norway@outlook.com>
2021-10-25 15:59:09 +0800

    开始编些快速排序

--- DataStructure/p2/quicksort.js
+++ DataStructure/p2/quicksort.js
@@ -1,8 +1,22 @@
 /*
- * @Descripttion: 
- * @version: 
+ * @Descripttion:
+ * @version:
  * @Author: Mirst
  * @Date: 2021-10-25 09:34:58
  * @LastEditors: Mirst
- * @LastEditTime: 2021-10-25 09:34:58
+ * @LastEditTime: 2021-10-25 09:48:17
  */
+
+/**
+ * @brief:
+ * @param {*} arr
+ * @return {*}
+ * @note: 相比合并排序，快排少了向上合并的过程，拆分完成后就是最终排好序的
+ *        空间复杂度相比合并排序低了很多
+ * @see:
+ */
+const quickSort = function quickSortA(arr) {
+  let array = [...arr];
+  let p = array.length - 1;
+  for (let i = 0; i < p; i++) {}
+};
