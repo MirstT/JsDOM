70dc2aa0951595557494d44d3cafc8eba045e5c7
MirstT <W_H_Norway@outlook.com>
2021-10-29 16:48:36 +0800

    理解闭包

--- scripts/closure.js
+++ scripts/closure.js
@@ -1,22 +1,23 @@
 /*
  * @Descripttion: 学习闭包
- * @version: 
+ * @version:
  * @Author: Mirst
  * @Date: 2021-10-15 09:15:54
- * @LastEditors: Mirst
- * @LastEditTime: 2021-10-15 10:33:09
+ * @LastEditors  : Mirst
+ * @LastEditTime : 2021-10-29 16:43:21
  */
 
-
-function funA(){
-    var a="sdas";
-    return function(){
-        console.log(a);
-    }
+function funA() {
+  var a = "sdas";
+  return function () {
+    console.log(a);
+  };
 }
 
-var b =funA();
+var b = funA();
 b();
+funA();
+funA()();
 
-console.log("dfsfdsf");
+
 
