0a50c816459c04f3bcc20c355c3a1f266f83c46e
Mirst <Mirst@outlook.com>
2021-11-22 16:06:10 +0800

    learn debug

---
+++ hello/app.js
@@ -0,0 +1,10 @@
+/*
+ * @Description  :
+ * @version      :
+ * @Author       : Mirst
+ * @Date         : 2021-11-22 15:08:53
+ * @LastEditors  : Mirst
+ * @LastEditTime : 2021-11-22 15:10:03
+ */
+var msg = "hello world";
+console.log(msg);
