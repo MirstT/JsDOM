258113c8538c041c94cfd23cb139926e862b9e4f
Mirst <Mirst@outlook.com>
2021-12-13 04:18:18 +0800

    优化文件结构

--- interview/datatype.js
+++ interview/datatype.js
@@ -1,6 +1,6 @@
 /*
- * @Descripttion : 
- * @version      : 
+ * @Descripttion :
+ * @version      :
  * @author       : Mirst
  * @Date         : 2021-11-07 23:43:02
  * @LastEditors  : Mirst
@@ -11,7 +11,7 @@ function Foo() {}
 var f1 = new Foo();
 var d = new Number(1);
 var s = "dsfa";
-var aa =new Number(11);
+var aa = new Number(11);
 var dd = aa;
 
 console.log(Function instanceof Object); // true
