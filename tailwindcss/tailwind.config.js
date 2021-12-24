e0271cf9c9f2f8f4c6b2422698951c15f94a0f69
Mirst <Mirst@outlook.com>
2021-12-24 22:38:42 +0800

    24

---
+++ tailwindcss/tailwind.config.js
@@ -0,0 +1,15 @@
+/*
+ * @Description  :
+ * @version      :
+ * @Author       : Mirst
+ * @Date         : 2021-12-22 22:57:58
+ * @LastEditors  : Mirst
+ * @LastEditTime : 2021-12-22 23:14:33
+ */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
